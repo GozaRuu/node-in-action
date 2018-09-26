0001e47daa8567782a31629a4e43fba3cd5cbc6d
GozaRuu <kais.sghari@gmail.com>
2018-09-26 17:57:45 +0100

    optimzed requests using fs.stat.size

--- static-file-server_node-fs-http/server.js
+++ static-file-server_node-fs-http/server.js
@@ -7,19 +7,20 @@ const pathResolve = require('path').resolve;
 const handleRequest = (req, res) => {
   const filepath = pathResolve('./public' + urlParse(req.url).pathname);
   console.log(`Request for file ${urlParse(req.url).pathname}`);
-  fs.stat(filepath, (err) => {
+  fs.stat(filepath, (err, stat) => {
     if (err) {
-      if (err.code == 'ENOENT') {
+      if (err.code == 'ENOENT') { //handle file not found byitself
         res.statusCode = 404;
         res.end(`<h1>ERROR 404: File Not Found</h1>`);
-      } else {
+      } else { //dump every other error here
         res.statusCode = 500;
         res.end(`<h1>ERROR 500: Internal Server Error</h1>`);
       }
     } else {
+      res.setHeader('Content-Length', stat.size); //optimize request speed by specifying the Content-Length beforehand using fs.stat
       const stream = fs.createReadStream(filepath)
       stream.pipe(res);
-      stream.on('error', (err) => {
+      stream.on('error', (err) => { //unexpected errors during piping the data from the file to the client
         res.statusCode = 500;
         res.end('<h1>ERROR 500: Internal Server Error</h1>');
       });
