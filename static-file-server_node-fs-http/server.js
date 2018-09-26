452f7699c96b6b11710e9c1b1ac5566d86634d07
GozaRuu <kais.sghari@gmail.com>
2018-09-26 17:49:57 +0100

    added more detailed error handling using fs.stat

--- static-file-server_node-fs-http/server.js
+++ static-file-server_node-fs-http/server.js
@@ -7,11 +7,23 @@ const pathResolve = require('path').resolve;
 const handleRequest = (req, res) => {
   const filepath = pathResolve('./public' + urlParse(req.url).pathname);
   console.log(`Request for file ${urlParse(req.url).pathname}`);
-  const stream = fs.createReadStream(filepath)
-  stream.pipe(res);
-  stream.on('error', (err) => {
-    res.statusCode = 500;
-    res.end('<h1>ERROR 500: Internal Server Error</h1>');
+  fs.stat(filepath, (err) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        res.statusCode = 404;
+        res.end(`<h1>ERROR 404: File Not Found</h1>`);
+      } else {
+        res.statusCode = 500;
+        res.end(`<h1>ERROR 500: Internal Server Error</h1>`);
+      }
+    } else {
+      const stream = fs.createReadStream(filepath)
+      stream.pipe(res);
+      stream.on('error', (err) => {
+        res.statusCode = 500;
+        res.end('<h1>ERROR 500: Internal Server Error</h1>');
+      });
+    }
   });
 };
 
