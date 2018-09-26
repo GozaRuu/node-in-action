e407dfb6420b7b5acfa28006db332df76d1ddf1f
GozaRuu <kais.sghari@gmail.com>
2018-09-26 17:40:55 +0100

    added minimal error handling

--- static-file-server_node-fs-http/server.js
+++ static-file-server_node-fs-http/server.js
@@ -7,7 +7,12 @@ const pathResolve = require('path').resolve;
 const handleRequest = (req, res) => {
   const filepath = pathResolve('./public' + urlParse(req.url).pathname);
   console.log(`Request for file ${urlParse(req.url).pathname}`);
-  fs.createReadStream(filepath).pipe(res);
+  const stream = fs.createReadStream(filepath)
+  stream.pipe(res);
+  stream.on('error', (err) => {
+    res.statusCode = 500;
+    res.end('<h1>ERROR 500: Internal Server Error</h1>');
+  });
 };
 
 const server = http.createServer(handleRequest);
