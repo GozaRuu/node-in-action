4a02353e681c008e52c869f4bfe80f387e671bfc
GozaRuu <kais.sghari@gmail.com>
2018-09-26 18:58:26 +0100

    added formidable to the project

---
+++ file-and-data-upload-server_node_http_formidable/readUserData.js
@@ -0,0 +1,1 @@
+const formidable = require('formidable');
--- file-and-data-upload-server_node_http_formidable/server.js
+++ file-and-data-upload-server_node_http_formidable/server.js
@@ -1,18 +1,17 @@
 const http = require('http');
 const displayForm = require('./displayForm');
+const readUserData = require('./readUserData');
 
 
 const handleRequest = (req, res) => {
   if (req.url =='/') {
     switch (req.method) {
       case 'GET':
-
         displayForm.display(res);
         break;
       case 'POST':
         displayForm.incrementCount(); //new contestant
-        // readUserInput(req, res);
-        res.end();
+        readUserInput(req, res);
         break;
       default:
         res.setStatus = 400;
