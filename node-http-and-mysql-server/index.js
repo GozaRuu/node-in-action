73c8e07c662c99f0399f65a2f85867f4c47e45ea
GozaRuu <kais.sghari@gmail.com>
2018-09-30 00:21:08 +0100

    node_mysql_project: added index.js which bootstrap the server and require the db

---
+++ node-http-and-mysql-server/index.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const mysql = require('mysql');
+const nodeMysql = require('./lib/nodeMysql');
+
+const HOST = 'localhost';
+const PORT = '3000';
+
+const db = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'nodemysql',
+  password: 'nodemysql',
+  database: 'nodemysql'
+});
+
+const handleRequest = (req, res) => {
+  switch (req.url) {
+
+    case '/':
+      switch (req.method) {
+        case 'GET':
+          nodeMysql.show(res);
+          break;
+        case 'POST':
+          nodeMysql.register(db, req, res);
+          break;
+        default:
+          res.end('<h1>ERROR 400: Unsupported Method</h1>');
+      }
+      break;
+
+    case '/show':
+      switch (req.method) {
+        case 'GET':
+          nodeMysql.showRegistered(db, req, res);
+          break;
+        default:
+          res.end('<h1>ERROR 400: Unsupported Method</h1>');
+      }
+      break;
+
+    case '/delete':
+      switch (req.method) {
+        case 'POST':
+          nodeMysql.delete(db, req, res);
+          break;
+        default:
+          res.end('<h1>ERROR 400: Unsupported Method</h1>');
+      }
+
+    default:
+      res.end('<h1>ERROR 404: File Not Found</h1>');
+  }
+};
+
+const server = http.createServer(handleRequest);
+server.listen(PORT, HOST, () => console.log(`app is now listening on port ${PORT}...`));
---
+++ node-http-and-mysql-server/lib/nodeMysql.js
@@ -0,0 +1,16 @@
+
+module.exports.show = (res) => {
+
+};
+
+module.exports.register = (db, req, res) => {
+
+};
+
+module.exports.showRegistered = (db, req, res) => {
+
+};
+
+module.exports.delete = (db, req, res) => {
+
+};
