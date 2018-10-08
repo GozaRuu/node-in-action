3fdd2c298aa7884f9769e02dd7c30c204c238185
GozaRuu <kais.sghari@gmail.com>
2018-10-08 18:24:05 +0100

    minor modifications

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -3,8 +3,28 @@ const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/jsjoe';
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url, { useNewUrlParser: true });
 
 connect.then((db) => {
     console.log('connected correctly to mongodb through mongoose');
+    const newDish = Dishes({
+        name: 'pizza',
+        description: 'pizza is awesome'
+    });
+
+    newDish.save()
+    .then((dish) => {
+        console.log(dish);
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
+        return Dishes.deleteMany({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 });
