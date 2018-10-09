30884a1bd4e833b9ebbcdadca8c31285f51e5ef5
GozaRuu <kais.sghari@gmail.com>
2018-10-09 14:03:45 +0100

    express-basicAuth: added basic authentification protocol implementation to express app

--- express-basicAuthentication/app.js
+++ express-basicAuthentication/app.js
@@ -21,8 +21,39 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+//basic authentification
+const basicAuth = (req, res, next) => {
+    console.log(req.headers);
+    const authHeader = req.headers.authorization; //will be in the form of : 'Basic YWRtaW46YWRtaW4='
+    if(!authHeader) { //this case have to handled separately
+        const err = new Error('Access not authorized');
+        res.setHeader('WWW-Authenticate', 'Basic'); //set authentication type to 'Basic'
+        err.status = 401;
+        return next(err);
+    }
+
+    try {
+        const auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':'); // take take the token and decode it from base64
+        //the decoded string is admin:admin split by ':' and check for user credential
+        const username = auth[0];
+        const password = auth[1];
+        if (username === 'admin' && password === 'admin') {
+            next();
+        } else {
+            const err = new Error('Access not authorized');
+            res.setHeader('WWW-Authenticate', 'Basic'); //set authentication type to 'Basic'
+            err.status = 401;
+            return next(err);
+        }
+    } catch (err) {
+        next(err);
+    }
+};
+app.use(basicAuth);
+
+//route
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
